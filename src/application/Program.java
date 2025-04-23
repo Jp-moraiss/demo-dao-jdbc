9c9b5d7d5b6966de4a95653abb275823288a31f1
Jp-moraiss <164018039+Jp-moraiss@users.noreply.github.com>
2025-04-23 00:11:19 -0300

    SellerDAOJDBC and DAOFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,11 +13,14 @@ public class Program {
 
 		Department obj = new Department(1, "Books");
 		
+		Seller seller = new Seller (21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+		SellerDAO sellerDao = DAOFactory.createSellerDAO();
+		
 		System.out.println(obj);
 		
-		Seller seller = new Seller (21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		
 		System.out.println(seller);
+		
 
 	}
 
